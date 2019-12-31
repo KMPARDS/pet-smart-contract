3a27114541cdaf93e1e3f36ccb0ec19a6fe9d8b9
zemse <sohamzemse143@gmail.com>
2019-12-31 17:34:22 +0530

    work on PET contract and write tests

---
+++ contracts/TimeAllyPET.sol
@@ -0,0 +1,369 @@
+pragma solidity ^0.6.0;
+
+import './SafeMath.sol';
+
+/*
+minimum is 50% of maximum deposited yet or 500 ES
+
+*/
+
+contract TimeAllyPET {
+  using SafeMath for uint256;
+
+  struct PETPlan {
+    bool isPlanActive;
+    uint256 minimumMonthlyCommitmentAmount;
+    uint256 monthlyBenefitFactorPerThousand;
+  }
+
+  struct PET {
+    uint256 planId;
+    uint256 initTimestamp;
+    uint256 lastAnnuityWithdrawlMonthId;
+    uint256 appointeeVotes;
+    uint256 numberOfAppointees;
+    mapping(uint256 => uint256) monthlyDepositAmount;
+    mapping(uint256 => bool) isPowerBoosterWithdrawn;
+    mapping(address => bool) nominees;
+    mapping(address => bool) appointees;
+  }
+
+  address public owner;
+  ERC20 public token;
+
+  uint256 constant EARTH_SECONDS_IN_MONTH = 2629744;
+
+  uint256 public pendingBenefitAmountOfAllStakers;
+  uint256 public fundsDeposit;
+
+  PETPlan[] public petPlans;
+  mapping(address => PET[]) public pets;
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, 'only deployer can call');
+    _;
+  }
+
+  modifier meOrNominee(address _stakerAddress, uint256 _petId) {
+    PET storage _pet = pets[_stakerAddress][_petId];
+
+    /// @notice if transacter is not staker, then transacter should be nominee
+    if(msg.sender != _stakerAddress) {
+      require(_pet.nominees[msg.sender], 'nomination should be there');
+    }
+    _;
+  }
+
+  constructor(ERC20 _token) public {
+    owner = msg.sender;
+    token = _token;
+  }
+
+  function createPETPlan(
+    uint256 _minimumMonthlyCommitmentAmount,
+    uint256 _monthlyBenefitFactorPerThousand
+  ) public onlyOwner {
+    petPlans.push(PETPlan({
+      isPlanActive: true,
+      minimumMonthlyCommitmentAmount: _minimumMonthlyCommitmentAmount,
+      monthlyBenefitFactorPerThousand: _monthlyBenefitFactorPerThousand
+    }));
+    // add an event here
+  }
+
+  function addFunds(uint256 _depositAmount) public {
+
+    /// @notice transfer tokens from the donor to contract
+    require(
+      token.transferFrom(msg.sender, address(this), _depositAmount)
+      , 'tokens should be transfered'
+    );
+
+    /// @notice increment amount in fundsDeposit
+    fundsDeposit = fundsDeposit.add(_depositAmount);
+
+    /// @notice emiting event that funds been deposited
+    // emit FundsDeposited(_depositAmount);
+  }
+
+  function withdrawFunds(uint256 _withdrawlAmount) public onlyOwner {
+
+    /// @notice check if withdrawing only unutilized tokens
+    require(
+      fundsDeposit.sub(pendingBenefitAmountOfAllStakers) >= _withdrawlAmount
+      , 'cannot withdraw excess funds'
+    );
+
+    /// @notice decrement amount in fundsDeposit
+    fundsDeposit = fundsDeposit.sub(_withdrawlAmount);
+
+    /// @notice transfer tokens to withdrawer
+    token.transfer(msg.sender, _withdrawlAmount);
+
+    /// @notice emit that funds are withdrawn
+    // emit FundsWithdrawn(_withdrawlAmount);
+  }
+
+
+  /// in new PET, it is better to also take a first deposit
+  function newPET(
+    uint256 _planId
+  ) public {
+    require(
+      petPlans[_planId].isPlanActive
+      , 'PET plan is not active'
+    );
+
+    pets[msg.sender].push(PET({
+      planId: _planId,
+      initTimestamp: now,
+      lastAnnuityWithdrawlMonthId: 0,
+      appointeeVotes: 0,
+      numberOfAppointees: 0
+    }));
+
+    // emit an event here
+  }
+
+  function getMonthlyDepositedAmount(
+    address _stakerAddress,
+    uint256 _petId,
+    uint256 _monthId
+  ) public view returns (uint256) {
+    return pets[_stakerAddress][_petId].monthlyDepositAmount[_monthId];
+  }
+
+  function getDepositMonth(
+    address _stakerAddress,
+    uint256 _petId
+  ) public view returns (uint256) {
+    return (now - pets[_stakerAddress][_petId].initTimestamp)/EARTH_SECONDS_IN_MONTH + 1;
+  }
+
+  function _getBenefitAllocationByDepositAmount(uint256 _amount, uint256 _planId) private returns (uint256) {
+    PETPlan storage _petPlan = petPlans[_planId];
+
+    // initialising benefit calculation with deposit amount
+    uint256 _benefitAllocation = _amount;
+
+    // if amount more than commitment, consider the deposit amount as double
+    if(_amount >= _petPlan.minimumMonthlyCommitmentAmount) {
+      _benefitAllocation = _benefitAllocation.mul(2);
+    }
+
+    // calculate the benefits in 5 years due to this deposit
+    _benefitAllocation = _benefitAllocation.mul(_petPlan.monthlyBenefitFactorPerThousand).mul(5).div(1000);
+
+    // adding extra amount in power booster
+    if(_amount >= _petPlan.minimumMonthlyCommitmentAmount) {
+      _benefitAllocation = _benefitAllocation.add(_amount);
+    }
+
+    return _benefitAllocation;
+  }
+
+  function makeDeposit(
+    address _stakerAddress,
+    uint256 _petId,
+    uint256 _depositAmount,
+    bool _usePrepaidES
+  ) public {
+    require(_depositAmount > 0, 'deposit amount should be non zero');
+
+    PET storage _pet = pets[_stakerAddress][_petId];
+    PETPlan storage _petPlan = petPlans[_pet.planId];
+
+    uint256 _depositMonth = getDepositMonth(_stakerAddress, _petId);
+
+    require(_depositMonth <= 12, 'cannot deposit after accumulation period');
+
+    token.transferFrom(msg.sender, address(this), _depositAmount);
+
+    uint256 _updatedDepositAmount = _pet.monthlyDepositAmount[_depositMonth].add(_depositAmount);
+
+    // also calculate old allocation, to adjust it in new allocation
+    uint256 _oldBenefitAllocation = _getBenefitAllocationByDepositAmount(
+      _pet.monthlyDepositAmount[_depositMonth],
+      _pet.planId
+    );
+    uint256 _extraBenefitAllocation = _getBenefitAllocationByDepositAmount(
+      _updatedDepositAmount,
+      _pet.planId
+    ).sub(_oldBenefitAllocation);
+
+    // here alocate funds for paying annuitity and power booster.
+    // also if fund crosses commitment then alocate more funds
+    require(
+      fundsDeposit.sub(pendingBenefitAmountOfAllStakers) >= _extraBenefitAllocation
+      , 'enough funds should exist'
+    );
+
+    pendingBenefitAmountOfAllStakers = pendingBenefitAmountOfAllStakers.add(_extraBenefitAllocation);
+
+    /// @dev recording the deposit
+    _pet.monthlyDepositAmount[_depositMonth] = _updatedDepositAmount;
+  }
+
+  // function getMonthlyBenefitAmount(
+  //   address _stakerAddress,
+  //   uint256 _petId,
+  //   uint256 _monthId
+  // ) public view returns (uint256) {
+  //   PET storage _pet = pets[_stakerAddress][_petId];
+  //   PETPlan storage _petPlan = petPlans[_pet.planId];
+  //
+  //   uint256 _modulo = _monthId%12;
+  //   uint256 _totalDepositAmount = _pet.monthlyDepositAmount[_modulo==0?12:_modulo];
+  //
+  //   if(_totalDepositAmount >= _petPlan.minimumMonthlyCommitmentAmount) {
+  //     _totalDepositAmount = _totalDepositAmount.mul(2);
+  //   }
+  //
+  //   return _totalDepositAmount.mul(_petPlan.monthlyBenefitFactorPerThousand).div(1000);
+  // }
+
+  function getSumOfMonthlyAnnuity(
+    address _stakerAddress,
+    uint256 _petId,
+    uint256 _startAnnuityMonthId,
+    uint256 _endAnnuityMonthId
+  ) public view returns (uint256) {
+    PET storage _pet = pets[_stakerAddress][_petId];
+    PETPlan storage _petPlan = petPlans[_pet.planId];
+
+    uint256 _sumOfAnnuity;
+
+    for(uint256 _i = _startAnnuityMonthId; _i <= _endAnnuityMonthId; _i++) {
+      uint256 _modulo = _i%12;
+      uint256 _depositDoneForThis = _pet.monthlyDepositAmount[_modulo==0?12:_modulo];
+
+      if(_depositDoneForThis >= _petPlan.minimumMonthlyCommitmentAmount) {
+        _depositDoneForThis = _depositDoneForThis.mul(2);
+      }
+
+      _sumOfAnnuity = _sumOfAnnuity.add(_depositDoneForThis);
+    }
+
+    return _sumOfAnnuity.mul(_petPlan.monthlyBenefitFactorPerThousand).div(1000);
+  }
+
+  function _getNomineeAllowedTimestamp(
+    address _stakerAddress,
+    uint256 _petId,
+    uint256 _annuityMonthId
+  ) private view returns (uint256) {
+    PET storage _pet = pets[_stakerAddress][_petId];
+    uint256 _allowedTimestamp = _pet.initTimestamp + (12 + _annuityMonthId - 1) * EARTH_SECONDS_IN_MONTH;
+
+    if(msg.sender != _stakerAddress) {
+      if(_pet.appointeeVotes > _pet.numberOfAppointees.div(2)) {
+        _allowedTimestamp += EARTH_SECONDS_IN_MONTH * 6;
+      } else {
+        _allowedTimestamp += EARTH_SECONDS_IN_MONTH * 12;
+      }
+    }
+
+    return _allowedTimestamp;
+  }
+
+  function withdrawAnnuity(
+    address _stakerAddress,
+    uint256 _petId,
+    uint256 _endAnnuityMonthId
+  ) public meOrNominee(_stakerAddress, _petId) {
+    PET storage _pet = pets[_stakerAddress][_petId];
+    uint256 _lastAnnuityWithdrawlMonthId = _pet.lastAnnuityWithdrawlMonthId;
+    // uint256 _currentAnnuityMonthId = ((now - _pet.initTimestamp).sub(12)) / EARTH_SECONDS_IN_MONTH;
+
+    require(
+      _lastAnnuityWithdrawlMonthId < _endAnnuityMonthId
+      , 'start should be before end'
+    );
+
+    require(
+      _endAnnuityMonthId <= 60
+      , 'only 60 Annuity withdrawls'
+    );
+
+    uint256 _allowedTimestamp = _getNomineeAllowedTimestamp(_stakerAddress, _petId, _endAnnuityMonthId);
+
+    require(
+      now >= _allowedTimestamp
+      , 'cannot withdraw early'
+    );
+
+    uint256 _annuityBenefit = getSumOfMonthlyAnnuity(
+      _stakerAddress,
+      _petId,
+      _lastAnnuityWithdrawlMonthId+1,
+      _endAnnuityMonthId
+    );
+
+    _pet.lastAnnuityWithdrawlMonthId = _endAnnuityMonthId;
+
+    if(_annuityBenefit != 0) {
+      pendingBenefitAmountOfAllStakers = pendingBenefitAmountOfAllStakers.add(_annuityBenefit);
+
+      fundsDeposit = fundsDeposit.sub(_annuityBenefit);
+
+      token.transfer(msg.sender, _annuityBenefit);
+    }
+
+    // emit an event here
+  }
+
+  function withdrawPowerBooster(
+    address _stakerAddress,
+    uint256 _petId,
+    uint256 _powerBoosterId
+  ) public meOrNominee(_stakerAddress, _petId) {
+    PET storage _pet = pets[_stakerAddress][_petId];
+    PETPlan storage _petPlan = petPlans[_pet.planId];
+
+    require(
+      1 <= _powerBoosterId && _powerBoosterId <= 12
+      , 'id should be in range'
+    );
+
+    require(
+      !_pet.isPowerBoosterWithdrawn[_powerBoosterId]
+      , 'booster already withdrawn'
+    );
+
+    require(
+      _pet.monthlyDepositAmount[13 - _powerBoosterId] >= _petPlan.minimumMonthlyCommitmentAmount
+      , 'target not achieved'
+    );
+
+    uint256 _allowedTimestamp = _getNomineeAllowedTimestamp(_stakerAddress, _petId, _powerBoosterId*5+1);
+
+    require(
+      now >= _allowedTimestamp
+      , 'cannot withdraw early'
+    );
+
+    uint256 _totalDeposited;
+
+    for(uint256 _i = 1; _i <= 12; _i++) {
+      uint256 _depositDoneForThis = _pet.monthlyDepositAmount[_i];
+
+      if(_depositDoneForThis >= _petPlan.minimumMonthlyCommitmentAmount) {
+        _depositDoneForThis = _depositDoneForThis.mul(2);
+      }
+
+      _totalDeposited = _totalDeposited.add(_depositDoneForThis);
+    }
+
+    _pet.isPowerBoosterWithdrawn[_powerBoosterId] = true;
+
+    uint256 _powerBoosterAmount = _totalDeposited.div(12);
+
+    token.transfer(msg.sender, _powerBoosterAmount);
+  }
+}
+
+/// @dev For interface requirement
+abstract contract ERC20 {
+  function transfer(address _to, uint256 _value) public virtual returns (bool success);
+  function transferFrom(address _from, address _to, uint256 _value) public virtual returns (bool success);
+}
---
+++ test/TimeAllyPET.test.js
@@ -0,0 +1,359 @@
+/*
+  Author: Soham Zemse (https://github.com/zemse)
+
+  In this file you should write tests for your smart contract as you progress in developing your smart contract. For reference of Mocha testing framework, you can check out https://devdocs.io/mocha/.
+*/
+
+/// @dev importing packages required
+const assert = require('assert');
+const ethers = require('ethers');
+const ganache = require('ganache-cli');
+
+/// @dev initialising development blockchain
+const provider = new ethers.providers.Web3Provider(ganache.provider({ gasLimit: 8000000 }));
+
+/// @dev importing build file
+const esJson = require('../build/Eraswap_ERC20Basic.json');
+const timeallyPETJSON = require('../build/TimeAllyPET_TimeAllyPET.json');
+
+/// @dev initialize global variables
+let accounts, esInstance, timeallyPETInstance;
+
+const increaseSeconds = 2629744;
+let evm_increasedTime = 2;
+
+const PETPlans = [
+  {minimumMonthlyCommitmentAmount: '500', monthlyBenefitFactorPerThousand: '100'},
+  {minimumMonthlyCommitmentAmount: '1000', monthlyBenefitFactorPerThousand: '105'},
+  {minimumMonthlyCommitmentAmount: '2500', monthlyBenefitFactorPerThousand: '110'},
+  {minimumMonthlyCommitmentAmount: '5000', monthlyBenefitFactorPerThousand: '115'},
+  {minimumMonthlyCommitmentAmount: '10000', monthlyBenefitFactorPerThousand: '120'}
+];
+
+const fundsDeposit = '20000';
+const petPlanId = 0;
+const depositCases = [
+  ['500'],
+  ['300'],
+  ['500'],
+  ['500'],
+  ['500'],
+  ['500'],
+  ['500'],
+  ['500'],
+  ['500'],
+  ['500'],
+  ['500'],
+  ['500']
+];
+
+let nextPowerBoosterWithdrawlMonthId = 1;
+
+/// @dev this is a test case collection
+describe('Ganache Setup', async() => {
+
+  /// @dev this is a test case. You first fetch the present state, and compare it with an expectation. If it satisfies the expectation, then test case passes else an error is thrown.
+  it('initiates ganache and generates a bunch of demo accounts', async() => {
+
+    /// @dev for example in this test case we are fetching accounts array.
+    accounts = await provider.listAccounts();
+
+    /// @dev then we have our expection that accounts array should be at least having 1 accounts
+    assert.ok(accounts.length >= 1, 'atleast 2 accounts should be present in the array');
+  });
+});
+
+describe('Eraswap Setup', () => {
+  it('deploys Eraswap ERC20 contract from first account', async() => {
+
+    /// @dev you create a contract factory for deploying contract. Refer to ethers.js documentation at https://docs.ethers.io/ethers.js/html/
+    const EraswapContractFactory = new ethers.ContractFactory(
+      esJson.abi,
+      esJson.evm.bytecode.object,
+      provider.getSigner(accounts[0])
+    );
+    esInstance =  await EraswapContractFactory.deploy();
+
+    assert.ok(esInstance.address, 'conract address should be present');
+  });
+});
+
+/// @dev this is another test case collection
+describe('TimeAllyPET Contract', () => {
+
+  /// @dev describe under another describe is a sub test case collection
+  describe('TimeAllyPET Setup', async() => {
+
+    /// @dev this is first test case of this collection
+    it('deploys TimeAllyPET contract from first account', async() => {
+
+      /// @dev you create a contract factory for deploying contract. Refer to ethers.js documentation at https://docs.ethers.io/ethers.js/html/
+      const TimeAllyPETContractFactory = new ethers.ContractFactory(
+        timeallyPETJSON.abi,
+        timeallyPETJSON.evm.bytecode.object,
+        provider.getSigner(accounts[0])
+      );
+      timeallyPETInstance =  await TimeAllyPETContractFactory.deploy(esInstance.address);
+
+      assert.ok(timeallyPETInstance.address, 'conract address should be present');
+    });
+
+    /// @dev this is second test case of this collection
+    it('value should be set properly while deploying', async() => {
+
+      /// @dev you access the value at storage with ethers.js library of our custom contract method called getValue defined in contracts/TimeAllyPET.sol
+      const ownerAddress = await timeallyPETInstance.functions.owner();
+      const tokenAddress = await timeallyPETInstance.functions.token();
+
+      /// @dev then you compare it with your expectation value
+      assert.equal(
+        ownerAddress,
+        accounts[0],
+        'owner address used while deploying must be visible when get'
+      );
+      assert.equal(
+        tokenAddress,
+        esInstance.address,
+        'token address set while deploying must be visible when get'
+      );
+    });
+
+    it('deployer sends 10,000 ES to account 1', async() => {
+      await esInstance.functions.transfer(
+        accounts[1],
+        ethers.utils.parseEther('10000')
+      );
+
+      const balanceOf1 = await esInstance.functions.balanceOf(accounts[1]);
+
+      assert.ok(balanceOf1.eq(ethers.utils.parseEther('10000')), 'account 1 should get 10,000 ES');
+    });
+
+    PETPlans.forEach((plan, index) => {
+      it('owner should be able to create a new PET Plan of minimum comitment '+plan.minimumMonthlyCommitmentAmount+' ES', async() => {
+        const minimumMonthlyCommitmentAmount = ethers.utils.parseEther(plan.minimumMonthlyCommitmentAmount);
+
+        await timeallyPETInstance.functions.createPETPlan(
+          minimumMonthlyCommitmentAmount,
+          plan.monthlyBenefitFactorPerThousand
+        );
+
+        const petPlan = await timeallyPETInstance.functions.petPlans(index);
+        // console.log(petPlan);
+
+        assert.ok(petPlan[0], 'plan should be active');
+        assert.ok(petPlan[1].eq(minimumMonthlyCommitmentAmount), 'minimumMonthlyCommitmentAmount should be properly set');
+        assert.ok(petPlan[2].eq(plan.monthlyBenefitFactorPerThousand), 'monthlyBenefitFactorPerThousand should be properly set');
+      });
+    });
+
+    it(`owner gives allowance of ${fundsDeposit} ES to PET contract`, async() => {
+      const approvalAmount = ethers.utils.parseEther(fundsDeposit);
+      await esInstance.functions.approve(timeallyPETInstance.address, approvalAmount);
+
+      const allowance = await esInstance.functions.allowance(accounts[0], timeallyPETInstance.address);
+
+      assert.ok(allowance.eq(approvalAmount), 'allowance should be set');
+    });
+
+    it(`owner should be able to fund ${fundsDeposit} ES to the fundDeposit`, async() => {
+      const balanceBefore = await esInstance.functions.balanceOf(accounts[0]);
+      const fundsDepositBefore = await timeallyPETInstance.functions.fundsDeposit();
+
+      const depositAmount = ethers.utils.parseEther(fundsDeposit);
+      const petId = 0;
+
+      await timeallyPETInstance.functions.addFunds(
+        depositAmount
+      );
+
+      const balanceAfter = await esInstance.functions.balanceOf(accounts[0]);
+      const fundsDepositAfter = await timeallyPETInstance.functions.fundsDeposit();
+
+      assert.ok(
+        fundsDepositAfter.sub(fundsDepositBefore).eq(depositAmount),
+        'increase in fundsDeposit should be deposit amount'
+      );
+
+      assert.ok(
+        balanceBefore.sub(balanceAfter).eq(depositAmount),
+        'balance difference should be deposit amount'
+      );
+    });
+
+  });
+
+  describe('TimeAllyPET New PET', async() => {
+    /// @dev this is first test case of this collection
+    it('Account 1 should be able to create new PET', async() => {
+      const _timeallyPETInstance = timeallyPETInstance.connect(provider.getSigner(accounts[1]));
+
+      const planId = petPlanId;
+
+      /// @dev you sign and submit a transaction to local blockchain (ganache) initialized on line 10.
+      await _timeallyPETInstance.functions.newPET(planId);
+
+      /// @dev now get the value at storage
+      const pet = await timeallyPETInstance.functions.pets(accounts[1], 0);
+
+      console.log(pet);
+
+      /// @dev then comparing with expectation value
+      assert.ok(
+        pet[0].eq(planId),
+        'plan id must be set properly'
+      );
+      assert.ok(
+        pet[1].gt(0),
+        'timestamp should be non zero'
+      );
+    });
+
+    depositCases.forEach((monthDepositArray, index) => {
+      describe(`Depositing during month ${index+1}`, async() => {
+        monthDepositArray.forEach(partDepositAmount => {
+          describe(`Account 1 makes deposit of ${partDepositAmount} ES to their PET`, async() => {
+            it('account 1 gives allowance of 500 ES to PET contract', async() => {
+              const _esInstance = esInstance.connect(provider.getSigner(accounts[1]));
+
+              const approvalAmount = ethers.utils.parseEther(partDepositAmount);
+              await _esInstance.functions.approve(timeallyPETInstance.address, approvalAmount);
+
+              const allowance = await _esInstance.functions.allowance(accounts[1], timeallyPETInstance.address);
+
+              assert.ok(allowance.eq(approvalAmount), 'allowance should be set');
+            });
+
+            it('account 1 should be able to make a deposit', async() => {
+              const _timeallyPETInstance = timeallyPETInstance.connect(provider.getSigner(accounts[1]));
+
+              const balanceBefore = await esInstance.functions.balanceOf(accounts[1]);
+              const monthlyDepositAmountBefore = await timeallyPETInstance.functions.getMonthlyDepositedAmount(accounts[1],0,index+1);
+              const allocatedFundsBefore = await timeallyPETInstance.functions.pendingBenefitAmountOfAllStakers();
+
+              const depositAmount = ethers.utils.parseEther(partDepositAmount);
+              const petId = 0;
+
+              await _timeallyPETInstance.functions.makeDeposit(
+                accounts[1], petId, depositAmount, false
+              );
+
+              const balanceAfter = await esInstance.functions.balanceOf(accounts[1]);
+              const monthlyDepositAmountAfter = await timeallyPETInstance.functions.getMonthlyDepositedAmount(accounts[1],0,index+1);
+              const allocatedFundsAfter = await timeallyPETInstance.functions.pendingBenefitAmountOfAllStakers();
+
+
+              const pet = await timeallyPETInstance.functions.pets(accounts[1], 0);
+
+              console.log('Addition to pending Benefits (annuitity and power booster):', ethers.utils.formatEther(allocatedFundsAfter.sub(allocatedFundsBefore)));
+
+              for(let i = 0; i <= 13; i++) {
+                console.log(i, ethers.utils.formatEther(await timeallyPETInstance.functions.getMonthlyDepositedAmount(accounts[1],0,i)));
+              }
+
+              assert.ok(
+                monthlyDepositAmountAfter.sub(monthlyDepositAmountBefore).eq(depositAmount),
+                'increase in monthly deposit should be deposit amount'
+              );
+
+              assert.ok(
+                balanceBefore.sub(balanceAfter).eq(depositAmount),
+                'balance difference should be deposit amount'
+              );
+            });
+          });
+        });
+        describe('TimeTravel', async() => {
+          it(`Traveling to future by ${increaseSeconds} seconds / about one month`, async() => {
+            evm_increasedTime += increaseSeconds;
+            const timeIncreased = await provider.send('evm_increaseTime', [increaseSeconds]);
+
+            assert.ok(Math.abs(timeIncreased - evm_increasedTime) < 10, 'increase in time should be one month');
+          });
+        });
+      });
+    });
+
+    describe('Benefits', async() => {
+      it('Seeing benefit', async() => {
+        for(let i = 1; i <= 30; i++) {
+          console.log(
+            i%12||12,
+            // ethers.utils.formatEther(await timeallyPETInstance.functions.getMonthlyDepositedAmount(accounts[1],0,i)),
+            // ethers.utils.formatEther(await timeallyPETInstance.functions.getMonthlyBenefitAmount(accounts[1],0,i)),
+            ethers.utils.formatEther(await timeallyPETInstance.functions.getSumOfMonthlyAnnuity(accounts[1],0,i,i)),
+          );
+        }
+      });
+
+      [...Array(60).keys()].map(n=>n+1).forEach(annuityMonthId => {
+        describe(`Benefit period ${annuityMonthId} month`, async() => {
+          describe('Annuity', async() => {
+            it(`Traveling to future by ${increaseSeconds} seconds / about one month`, async() => {
+              evm_increasedTime += increaseSeconds;
+              const timeIncreased = await provider.send('evm_increaseTime', [increaseSeconds]);
+
+              assert.ok(Math.abs(timeIncreased - evm_increasedTime) < 10, 'increase in time should be one month');
+            });
+
+            // if(annuityMonthId%12) {
+            //   return;
+            // }
+
+            it(`Withdrawing Annuity for Benefit Month ${annuityMonthId} or Actual ${annuityMonthId+12}`, async() => {
+              const _timeallyPETInstance = timeallyPETInstance.connect(provider.getSigner(accounts[1]));
+
+              const balanceBefore = await esInstance.functions.balanceOf(accounts[1]);
+
+              await _timeallyPETInstance.functions.withdrawAnnuity(
+                accounts[1],
+                0,
+                annuityMonthId
+              );
+
+              const balanceAfter = await esInstance.functions.balanceOf(accounts[1]);
+
+              console.log(`AN ${annuityMonthId}/${annuityMonthId}) Received:`, ethers.utils.formatEther(balanceAfter.sub(balanceBefore)));
+
+            });
+          });
+
+          if(annuityMonthId%5===0 && nextPowerBoosterWithdrawlMonthId+1 <= 12){
+            describe(`Power Booster`, async() => {
+              it(`Powerbooster Withdrawl during ${annuityMonthId}`, async() => {
+                console.log('\t\tPower Booster Withdrawl ID: '+ nextPowerBoosterWithdrawlMonthId);
+                const _timeallyPETInstance = timeallyPETInstance.connect(provider.getSigner(accounts[1]));
+
+                const balanceBefore = await esInstance.functions.balanceOf(accounts[1]);
+
+                try {
+                  await _timeallyPETInstance.functions.withdrawPowerBooster(
+                    accounts[1],
+                    0,
+                    nextPowerBoosterWithdrawlMonthId
+                  );
+
+                  const balanceAfter = await esInstance.functions.balanceOf(accounts[1]);
+
+                  console.log(`PB ${nextPowerBoosterWithdrawlMonthId}/${annuityMonthId}) Received:`, ethers.utils.formatEther(balanceAfter.sub(balanceBefore)));
+
+                  nextPowerBoosterWithdrawlMonthId++;
+                } catch(error) {
+                  // console.log(error.message, error.message.includes('target not achieved'));
+                  if(error.message.includes('target not achieved')) {
+                    // console.log(nextPowerBoosterWithdrawlMonthId);
+                    nextPowerBoosterWithdrawlMonthId++;
+                    // console.log(nextPowerBoosterWithdrawlMonthId);
+                  }
+                  throw error;
+                }
+              });
+            });
+          }
+        });
+
+      });
+    });
+
+  });
+});
